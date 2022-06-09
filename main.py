58861c696b50c523da782740b3b71e685f4f1c74
Lukypop <91247831+Lukypop@users.noreply.github.com>
2022-06-09 02:03:45 +0200

    Celé jsem to dělal sám :D

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 speedFactor = 80
-pin_L = DigitalPin.P13
+servo = PCAmotor.Servos.S1
+pin_L = DigitalPin.P1
 pin_R = DigitalPin.P14
 pin_Trig = DigitalPin.P8
 pin_Echo = DigitalPin.P15
-whiteline = 1
+
+whiteline = 0
 connected = 0
+start = False
+last = 0
+
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 pins.set_pull(pin_L, PinPullMode.PULL_NONE)
 pins.set_pull(pin_R, PinPullMode.PULL_NONE)
 bluetooth.start_uart_service()
 basic.show_string("S")
 
-# temporary code
-motor_run(100, 100); basic.pause(2000)
-motor_run(); basic.pause(300)
-motor_run(-100, -100, 60); basic.pause(2000)
-motor_run()
+input.on_button_pressed(Button.A, robot_run)
 
-strip.set_pixel_color(0, neopixel.hsl(0, 50, 50)) # hmax = 360, smax = 100, lmax = 50
-strip.set_pixel_color(3, neopixel.hsl(140, 100, 25))
-strip.show()
-# end of temporary code
-
+def robot_run():
+    global start 
+    if start == True: 
+        start = False
+        print("stojim pane")
+    else: 
+        start = True
+        print("jedu pane")
 def motor_run(left = 0, right = 0, speed_factor = 80):
-    PCAmotor.motor_run(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
-    PCAmotor.motor_run(PCAmotor.Motors.M4, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.motor_run(PCAmotor.Motors.M2, Math.map(Math.constrain(-1 * left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.motor_run(PCAmotor.Motors.M3, Math.map(Math.constrain(right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
 
 def on_bluetooth_connected():
     global connected
@@ -42,12 +46,35 @@ def on_bluetooth_disconnected():
 bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
 
 def on_forever():
+    global last
+    speed = 60
+    reverse_speed = -12
+    factor = 75
     obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.CENTIMETERS, 100)
+    if start:
 
-    l = False if (whiteline ^ pins.digital_read_pin(pin_L)) == 0 else True
-    r = False if (whiteline ^ pins.digital_read_pin(pin_R)) == 0 else True
+        l = False if (whiteline ^ pins.digital_read_pin(pin_L)) == 0 else True
+        r = False if (whiteline ^ pins.digital_read_pin(pin_R)) == 0 else True
 
-    # TO DO ...
-
-    basic.pause(50) #reakční frekvence 20 Hz
+        #console.log_value("left", l)
+        #console.log_value("right", r)
+        #basic.pause(200)
+        
+        
+        if l and r:
+            motor_run(speed, speed, factor)
+        elif l and not r:
+            motor_run(speed, reverse_speed, factor)
+            last = 0
+        elif r and not l:
+            motor_run(reverse_speed, speed, factor)
+            last = 1
+        else:
+            if last == 0:
+                motor_run(reverse_speed, speed, factor)
+            else:
+                motor_run(speed, reverse_speed, factor)    
+    else:
+        motor_run()
+    basic.pause(10) #reakční frekvence 20 Hz
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,31 +1,32 @@
 let speedFactor = 80
-let pin_L = DigitalPin.P13
+let servo = PCAmotor.Servos.S1
+let pin_L = DigitalPin.P1
 let pin_R = DigitalPin.P14
 let pin_Trig = DigitalPin.P8
 let pin_Echo = DigitalPin.P15
-let whiteline = 1
+let whiteline = 0
 let connected = 0
+let start = false
+let last = 0
 let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 pins.setPull(pin_L, PinPullMode.PullNone)
 pins.setPull(pin_R, PinPullMode.PullNone)
 bluetooth.startUartService()
 basic.showString("S")
-//  temporary code
-motor_run(100, 100)
-basic.pause(2000)
-motor_run()
-basic.pause(300)
-motor_run(-100, -100, 60)
-basic.pause(2000)
-motor_run()
-strip.setPixelColor(0, neopixel.hsl(0, 50, 50))
-//  hmax = 360, smax = 100, lmax = 50
-strip.setPixelColor(3, neopixel.hsl(140, 100, 25))
-strip.show()
-//  end of temporary code
+input.onButtonPressed(Button.A, function robot_run() {
+    
+    if (start == true) {
+        start = false
+        console.log("stojim pane")
+    } else {
+        start = true
+        console.log("jedu pane")
+    }
+    
+})
 function motor_run(left: number = 0, right: number = 0, speed_factor: number = 80) {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.MotorRun(PCAmotor.Motors.M2, Math.map(Math.constrain(-1 * left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.MotorRun(PCAmotor.Motors.M3, Math.map(Math.constrain(right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
 }
 
 bluetooth.onBluetoothConnected(function on_bluetooth_connected() {
@@ -45,9 +46,36 @@ bluetooth.onBluetoothDisconnected(function on_bluetooth_disconnected() {
 })
 // reakční frekvence 20 Hz
 basic.forever(function on_forever() {
+    let l: any;
+    let r: any;
+    
+    let speed = 60
+    let reverse_speed = -12
+    let factor = 75
     let obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.Centimeters, 100)
-    let l = (whiteline ^ pins.digitalReadPin(pin_L)) == 0 ? false : true
-    let r = (whiteline ^ pins.digitalReadPin(pin_R)) == 0 ? false : true
-    //  TO DO ...
-    basic.pause(50)
+    if (start) {
+        l = (whiteline ^ pins.digitalReadPin(pin_L)) == 0 ? false : true
+        r = (whiteline ^ pins.digitalReadPin(pin_R)) == 0 ? false : true
+        // console.log_value("left", l)
+        // console.log_value("right", r)
+        // basic.pause(200)
+        if (l && r) {
+            motor_run(speed, speed, factor)
+        } else if (l && !r) {
+            motor_run(speed, reverse_speed, factor)
+            last = 0
+        } else if (r && !l) {
+            motor_run(reverse_speed, speed, factor)
+            last = 1
+        } else if (last == 0) {
+            motor_run(reverse_speed, speed, factor)
+        } else {
+            motor_run(speed, reverse_speed, factor)
+        }
+        
+    } else {
+        motor_run()
+    }
+    
+    basic.pause(10)
 })
